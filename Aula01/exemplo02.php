1ada2b89a69966451779db7dbc4a6b32c684e881
Ramon150908 <163332543+Ramon150908@users.noreply.github.com>
2026-02-10 17:31:59 -0300

    Add files via upload

---
+++ Aula01/exemplo02.php
@@ -0,0 +1,11 @@
+<?php
+    $nome = "Ramon";
+    $idade = 17;
+    $preco = 38.69;
+    $ativo = true;
+    echo"Nome: ".$nome." <br>";
+    echo"Idade: ". $idade . "<br>";
+    echo"Preço: ". $preco ."<br>";
+    echo"Status: ". ($ativo ? "Ativo" : "Inativo");
+
+?>
